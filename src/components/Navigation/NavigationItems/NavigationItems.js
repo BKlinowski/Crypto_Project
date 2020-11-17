00305dca4c6bd6140e68a9f1944e9c4a6fb66f47
Bartosz Klinowski <bartoszklinowski@gmail.com>
2020-11-17 02:27:49 +0100

    Small changes.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,17 +8,21 @@ const navigationItems = (props) => {
     <div className={styles.NavigationItems}>
       <div className={styles.Submenu}>
         <NavigationItem text="Encrypt" />
-        <NavigationSubitem text="AES" />
-        <NavigationSubitem text="RSA" />
-        <NavigationSubitem text="Blowfish" />
-        <NavigationSubitem text="Twofish" />
-        <NavigationSubitem text="Camellia" />
-        <NavigationSubitem text="Salsa20" />
+        <div className={styles.Items}>
+          <NavigationSubitem text="AES" />
+          <NavigationSubitem text="RSA" />
+          <NavigationSubitem text="Blowfish" />
+          <NavigationSubitem text="Twofish" />
+          <NavigationSubitem text="Camellia" />
+          <NavigationSubitem text="Salsa20" />
+        </div>
       </div>
       <div className={styles.Submenu}>
         <NavigationItem text="Hash" />
-        <NavigationSubitem text="SHA-256" />
-        <NavigationSubitem text="MD5" />
+        <div className={styles.Items}>
+          <NavigationSubitem text="SHA-256" />
+          <NavigationSubitem text="MD5" />
+        </div>
       </div>
     </div>
   );
