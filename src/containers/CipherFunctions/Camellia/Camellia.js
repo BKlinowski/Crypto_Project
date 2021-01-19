91cae7f7d421ca43b24e614e438d7544a59e2156
Bartosz Klinowski <bartoszklinowski@gmail.com>
2021-01-19 23:55:00 +0100

    Small changes camellia

--- src/containers/CipherFunctions/Camellia/Camellia.js
+++ src/containers/CipherFunctions/Camellia/Camellia.js
@@ -5,6 +5,7 @@ import OptionCamellia from "../../../components/Options/Option/OptionCamellia/Op
 import Camellia from "../../../scripts/camellia/camellia";
 
 import camelliaFunction from "../../../scripts/camellia/camellia";
+import modeOfOperation from "../../../scripts/ModeOfOperation";
 
 const AES = (props) => {
   const [areaValue, setAreaValue] = useState("");
@@ -13,7 +14,10 @@ const AES = (props) => {
   const [buttonText, setButtonText] = useState("Current: encryption");
   const [camelliaResult, setCamelliaResult] = useState("");
   const [camellia, setCamellia] = useState(null);
-  const [areaMaxVal, setAreaMaxVal] = useState(16);
+  const [areaMaxVal, setAreaMaxVal] = useState();
+  const [modeValue, setModeValue] = useState("ECB");
+  const [nonceValue, setNonceValue] = useState("");
+  const [nonceMode, setNonceMode] = useState("");
 
   useEffect(() => {
     if (inputValue.length == 32) {
@@ -25,6 +29,63 @@ const AES = (props) => {
   }, [inputValue]);
 
   useEffect(() => {
+    console.log(modeValue);
+    if (inputValue.length !== 32) {
+      setCamelliaResult("Key must be 32 characters length!");
+    } else if (modeValue !== "ECB" && nonceValue.length !== 8) {
+      setCamelliaResult("Nonce value must be 8 characters length!");
+    } else {
+      if (switchMode) {
+        try {
+          console.log(nonceValue);
+          setCamelliaResult(
+            modeOfOperation.encrypt(
+              areaValue,
+              (message) => {
+                return camellia.encrypt(areaValue);
+              },
+              (ciphertext) => {
+                return camellia.decrypt(areaValue);
+              },
+              {
+                paddingType: modeOfOperation.PADDING_TYPE.ISO10126_2,
+                modeOfOperation: modeOfOperation.MODE.nonceMode,
+                nonce: nonceValue,
+              }
+            )
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        try {
+          setCamelliaResult(
+            modeOfOperation.decrypt(
+              areaValue
+                .split(",")
+                .map((char) => +char)
+                .flat(),
+              (message) => {
+                return camellia.encrypt(areaValue);
+              },
+              (ciphertext) => {
+                return camellia.decrypt(areaValue);
+              },
+              {
+                paddingType: modeOfOperation.PADDING_TYPE.ISO10126_2,
+                modeOfOperation: modeOfOperation.MODE.nonceMode,
+                nonce: nonceValue,
+              }
+            )
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  }, [areaValue, inputValue, nonceValue, modeValue, switchMode]);
+
+  /* useEffect(() => {
     if (switchMode && inputValue && areaValue) {
       setCamelliaResult(camellia.encrypt(areaValue));
     } else if (!switchMode && inputValue && areaValue) {
@@ -32,7 +93,46 @@ const AES = (props) => {
     } else {
       setCamelliaResult("Just type some characters");
     }
-  }, [areaValue]);
+  }, [areaValue]); */
+
+  useEffect(() => {
+    // console.log(nonceMode);
+    switch (modeValue) {
+      case "ECB": {
+        setNonceMode("");
+        break;
+      }
+      case "CBC": {
+        setNonceMode("IV");
+        break;
+      }
+      case "CFB": {
+        setNonceMode("IV");
+        break;
+      }
+      case "OFB": {
+        setNonceMode("IV");
+        break;
+      }
+      case "CTR": {
+        setNonceMode("nonce");
+        break;
+      }
+      default: {
+        setNonceMode("");
+        break;
+      }
+    }
+  }, [modeValue, setNonceMode]);
+
+  const onNonceChange = (event) => {
+    setNonceValue(event.target.value);
+  };
+
+  const onModeChange = (event) => {
+    setCamelliaResult("");
+    setModeValue(event.target.value);
+  };
 
   const onTextAreaChange = (event) => {
     setAreaValue(event.target.value);
@@ -45,11 +145,13 @@ const AES = (props) => {
   const onButtonClick = () => {
     if (!switchMode) {
       setAreaValue("");
-      setAreaMaxVal(16);
+      setNonceValue("");
+      setCamelliaResult("");
       setButtonText("Current: encryption");
     } else {
       setAreaValue("");
-      setAreaMaxVal(32);
+      setNonceValue("");
+      setCamelliaResult("");
       setButtonText("Current: decryption");
     }
     setSwitchMode(!switchMode);
@@ -57,7 +159,7 @@ const AES = (props) => {
 
   return (
     <DefaultMain max={areaMaxVal} onTextAreaChange={onTextAreaChange} areaValue={areaValue} result={camelliaResult}>
-      <OptionCamellia switchMode={switchMode} switchModeText={buttonText} inputValue={inputValue} onInputChange={onInputChange} onButtonClick={onButtonClick} />
+      <OptionCamellia switchMode={switchMode} switchModeText={buttonText} inputValue={inputValue} onInputChange={onInputChange} onButtonClick={onButtonClick} onModeChange={onModeChange} modeValue={modeValue} nonceMode={nonceMode} onNonceChange={onNonceChange} />
     </DefaultMain>
   );
 };
